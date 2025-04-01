217ca02a786b288a69eb8ba298598615b995a773
Leo <leo2012.c@gmail.com>
2025-04-01 14:48:42 +0800

    ✅ 調整組件路徑

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { LoginForm } from "./login-form";
+import { LoginForm } from "../../components/login/login-form";
 
 export default function LoginPage() {
   return (
